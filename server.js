ca9945e7140b334ae28ec983f76426ebf8752b3b
aclar <a.clarke0307@gmail.com>
2021-09-11 15:32:38 -0400

    create queries

--- server.js
+++ server.js
@@ -20,10 +20,38 @@ const db = mysql.createConnection(
     console.log('Connect to the election database')
 );
 
+//request a list of all potential candidates
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+   // console.log(rows);
+//})
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+//GET a single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row)=> {
+ //   if(err) {
+   //     console.log(err);
+  //  }
+  //  console.log(row);
+//});
+
+//DELETE a candidate
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) =>{
+  //  if(err){
+    //    console.log(err);
+    //}
+    //console.log(result);
+//});
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
 // Default response for any other request (Not Found) place below an app.get also called a catchall
 app.use((req, res) => {
