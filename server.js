03498bd84938bd737cbb29d4aadb7d9a4e4dfad6
aclar <a.clarke0307@gmail.com>
2021-09-11 15:04:42 -0400

    database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +7,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MYSQL username
+        user: 'root',
+        // Your Mysql password
+        password: 'Thinkpadchester1!',
+        database: 'election'
+    },
+    console.log('Connect to the election database')
+);
 
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
-
-
-// Default response for any other request (Not Found) place below an app.get
+// Default response for any other request (Not Found) place below an app.get also called a catchall
 app.use((req, res) => {
     res.status(404).end();
 });
